860b614aa2ac49dc929d5ad6fa4482c7c3f51bb3
Michael Fuget <michaelfuget@gmail.com>
2021-01-28 06:25:20 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def display_artist(song)
+    if !song.artist.blank?
+      "<%= link_to \"Artist\", artist_path(song.artist)%>"
+    else
+      "<%= link_to \"edit song\", song_path(song)%>"
+    end
+  end
 end
+<%= link_to "Add New Song", song_path() %>
